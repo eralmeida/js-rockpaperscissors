ecf4ceb473d5080790506e24c4ff5dd98665077a
Eduardo Almeida <edu.almeida8@gmail.com>
2023-03-31 13:32:21 +0100

    Assignment - Javascript

--- script.js
+++ script.js
@@ -1,59 +1,87 @@
+var score = 0
+var plays = 0
+const buttons = document.querySelectorAll('button')
+buttons.forEach((button) => {
+  button.addEventListener('mousedown', (event) => {
+    if (event.button == 0) {
+      //Add Font Awesome animation
+      let icon = button.querySelector('i')
+      icon.classList.add('fa-beat')
+      setTimeout(() => {
+        icon.classList.remove('fa-beat')
+      }, 800)
+    }
+  }),
+    button.addEventListener('mouseup', roundPlayHandler)
+})
+
+function cleanupListeners() {
+  const buttons = document.querySelectorAll('button')
+  buttons.forEach((button) => button.removeEventListener('mouseup', roundPlayHandler))
+}
+
+function endGame() {
+  let result = ''
+  if (score === 0) {
+    result = "It's a tie!"
+  }
+  if (score < 0) {
+    result = 'You win!'
+  }
+  if (score > 0) {
+    result = 'You lose!'
+  }
+  document.querySelector('.final-result').textContent = result
+}
+
 function getComputerChoice() {
   let choiceArray = ['Rock', 'Paper', 'Scissors']
   return (choiceArray = choiceArray[Math.floor(Math.random() * choiceArray.length)])
 }
 
-function playRound(playerOne, playerTwo) {
-  if (playerOne === playerTwo) {
+function drawRoundPickItem(playerChoice, computerChoice) {
+  let pickClasses = {
+    rock: 'fa-regular fa-solid fa-hand-back-fist fa-fw fa-xl',
+    paper: 'fa-regular fa-solid fa-hand-paper fa-fw fa-xl',
+    scissors: 'fa-regular fa-solid fa-hand-scissors fa-fw fa-xl',
+  }
+
+  let listOfRounds = document.querySelector('.rounds')
+  let listItem = document.createElement('li')
+
+  const html = `<div>You <i class='${pickClasses[playerChoice]}'></i></div> <div> <i class='${pickClasses[computerChoice]}'></i> Computer</div>`
+
+  listItem.insertAdjacentHTML('beforeend', html)
+  listOfRounds.appendChild(listItem)
+}
+
+function playRound(player, computer) {
+  if (player === computer) {
     return 0
   }
-
-  if (playerOne === 'rock') {
-    return playerTwo === 'scissors' ? -1 : 1
+  if (player === 'rock') {
+    return computer === 'scissors' ? -1 : 1
   }
-
-  if (playerOne === 'scissors') {
-    return playerTwo === 'paper' ? -1 : 1
+  if (player === 'scissors') {
+    return computer === 'paper' ? -1 : 1
   }
-
-  if (playerOne === 'paper') {
-    return playerTwo === 'rock' ? -1 : 1
+  if (player === 'paper') {
+    return computer === 'rock' ? -1 : 1
   }
 }
 
-function game() {
-  let finalResult = 0
+function roundPlayHandler(event) {
+  if (event.button == 0) {
+    let playerChoice = this.getAttribute('data-pick')
+    let computerChoice = getComputerChoice().toLowerCase()
 
-  for (let i = 1; i < 6; i++) {
-    let playerChoice = prompt('Pick your play')
-    let computerChoice = getComputerChoice()
-    playerChoice = playerChoice.toLowerCase()
-    computerChoice = computerChoice.toLowerCase()
+    drawRoundPickItem(playerChoice, computerChoice)
+    score += playRound(playerChoice, computerChoice)
+    plays += 1
 
-    let roundResult = playRound(playerChoice, computerChoice)
-
-    if (roundResult === 0) {
-      console.log('Round draw!', playerChoice, computerChoice)
+    if (plays == 5) {
+      cleanupListeners()
+      endGame()
     }
-    if (roundResult === 1) {
-      console.log('You lose this round!', playerChoice, computerChoice)
-    }
-    if (roundResult === -1) {
-      console.log('You win this round!', playerChoice, computerChoice)
-    }
-
-    finalResult += roundResult
-  }
-
-  if (finalResult === 0) {
-    console.log("It's a draw!")
-  }
-  if (finalResult > 0) {
-    console.log('You lost the game')
-  }
-  if (finalResult < 0) {
-    console.log('You win the game!')
   }
 }
-
-game()
